6839695facc364a964f08fced2b1d42c1a6b220f
Gabriel Henrique <Gabriel.Henrique@senior.com.br>
2021-12-21 17:14:43 -0300

    frontgato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { GatoComponent } from 'src/gato/gato.component';
+import { GatoRegisterComponent } from 'src/register/gato-register.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GatoComponent,
+    GatoRegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
